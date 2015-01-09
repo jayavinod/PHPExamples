26046a00ecf75780af4aac81d16ff7f0ef062dc2
jayavinod <jayalakshmiv@aditi.com>
2015-01-09 11:53:52 +0530

    first checkin

---
+++ prime.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(E_ALL);
+//Program to find prime numbers in between a range
+
+$num =23;
+
+for( $j = 2; $j <= $num; $j++ )
+{
+	for( $k = 2; $k < $j; $k++ )
+	{
+		echo $j."J====K".$k;
+		
+		if( $j % $k == 0 )
+		{
+			break;
+		}
+	}	
+if($k == $j)
+echo "Prime Number :". $j."<br/>";
+}
+?>
